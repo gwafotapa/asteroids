bb88dda56aca3a2b8890d154ecd9f8ecb4238e26
Guillaume <gwafotapa@gmail.com>
2022-11-05 14:59:30 +0100

    Add test for is_point_in_triangle and correct a bug

---
+++ src/boss.rs
@@ -0,0 +1,109 @@
+use bevy::prelude::*;
+use std::f32::consts::{PI, SQRT_2};
+
+const BOSS_INNER_RADIUS: f32 = 100.0;
+const BOSS_OUTER_RADIUS: f32 = BOSS_INNER_RADIUS * SQRT_2;
+
+/// Counter clockwise
+pub const BOSS_POLYGON: [Vec3; 16] = [
+    Vec3 {
+        x: -BOSS_OUTER_RADIUS,
+        y: 0.0,
+        z: 0.0,
+    },
+    Vec3 {
+        x: -BOSS_INNER_RADIUS,
+        y: BOSS_INNER_RADIUS - BOSS_OUTER_RADIUS,
+        z: 0.0,
+    },
+    Vec3 {
+        x: -BOSS_INNER_RADIUS,
+        y: -BOSS_INNER_RADIUS,
+        z: 0.0,
+    },
+    Vec3 {
+        x: BOSS_INNER_RADIUS - BOSS_OUTER_RADIUS,
+        y: -BOSS_INNER_RADIUS,
+        z: 0.0,
+    },
+    Vec3 {
+        x: 0.0,
+        y: -BOSS_OUTER_RADIUS,
+        z: 0.0,
+    },
+    Vec3 {
+        x: BOSS_OUTER_RADIUS - BOSS_INNER_RADIUS,
+        y: -BOSS_INNER_RADIUS,
+        z: 0.0,
+    },
+    Vec3 {
+        x: BOSS_INNER_RADIUS,
+        y: -BOSS_INNER_RADIUS,
+        z: 0.0,
+    },
+    Vec3 {
+        x: BOSS_INNER_RADIUS,
+        y: BOSS_INNER_RADIUS - BOSS_OUTER_RADIUS,
+        z: 0.0,
+    },
+    Vec3 {
+        x: BOSS_OUTER_RADIUS,
+        y: 0.0,
+        z: 0.0,
+    },
+    Vec3 {
+        x: BOSS_INNER_RADIUS,
+        y: BOSS_OUTER_RADIUS - BOSS_INNER_RADIUS,
+        z: 0.0,
+    },
+    Vec3 {
+        x: BOSS_INNER_RADIUS,
+        y: BOSS_INNER_RADIUS,
+        z: 0.0,
+    },
+    Vec3 {
+        x: BOSS_OUTER_RADIUS - BOSS_INNER_RADIUS,
+        y: BOSS_INNER_RADIUS,
+        z: 0.0,
+    },
+    Vec3 {
+        x: 0.0,
+        y: BOSS_OUTER_RADIUS,
+        z: 0.0,
+    },
+    Vec3 {
+        x: BOSS_INNER_RADIUS - BOSS_OUTER_RADIUS,
+        y: BOSS_INNER_RADIUS,
+        z: 0.0,
+    },
+    Vec3 {
+        x: -BOSS_INNER_RADIUS,
+        y: BOSS_INNER_RADIUS,
+        z: 0.0,
+    },
+    Vec3 {
+        x: -BOSS_INNER_RADIUS,
+        y: BOSS_OUTER_RADIUS - BOSS_INNER_RADIUS,
+        z: 0.0,
+    },
+];
+
+pub fn create_triangle_list_from_polygon(polygon: &[Vec3], center: Vec3) -> Vec<Vec3> {
+    let mut triangle_list = Vec::new();
+    let mut iter = polygon.iter();
+    let mut p1 = iter.next();
+    let mut p2 = iter.next();
+    let p0 = p1;
+    while p2.is_some() {
+        triangle_list.push(center);
+        triangle_list.push(*p1.unwrap());
+        triangle_list.push(*p2.unwrap());
+        p1 = p2;
+        p2 = iter.next();
+    }
+    triangle_list.push(center);
+    triangle_list.push(*p1.unwrap());
+    triangle_list.push(*p0.unwrap());
+
+    triangle_list
+}
---
+++ tests/tests.rs
@@ -0,0 +1,96 @@
+use asteroids::*;
+use bevy::prelude::*;
+
+const O: Vec3 = Vec3::ZERO;
+const A: Vec3 = Vec3 {
+    x: -2.0,
+    y: -1.0,
+    z: 0.0,
+};
+const B: Vec3 = Vec3 {
+    x: -1.0,
+    y: 5.0,
+    z: 0.0,
+};
+const C: Vec3 = Vec3 {
+    x: 9.0,
+    y: 2.0,
+    z: 0.0,
+};
+const D: Vec3 = Vec3 {
+    x: -1.0,
+    y: 4.0,
+    z: 0.0,
+};
+const E: Vec3 = Vec3 {
+    x: -2.0,
+    y: 0.0,
+    z: 0.0,
+};
+const F: Vec3 = Vec3 {
+    x: 5.0,
+    y: 1.0,
+    z: 0.0,
+};
+const G: Vec3 = Vec3 {
+    x: 8.0,
+    y: 2.0,
+    z: 0.0,
+};
+const H: Vec3 = Vec3 {
+    x: 2.0,
+    y: 4.0,
+    z: 0.0,
+};
+const I: Vec3 = Vec3 {
+    x: 6.0,
+    y: 3.0,
+    z: 0.0,
+};
+const J: Vec3 = Vec3 {
+    x: -4.0,
+    y: 3.0,
+    z: 0.0,
+};
+const K: Vec3 = Vec3 {
+    x: -2.0,
+    y: -6.0,
+    z: 0.0,
+};
+const L: Vec3 = Vec3 {
+    x: 11.0,
+    y: -9.0,
+    z: 0.0,
+};
+const M: Vec3 = Vec3 {
+    x: 8.0,
+    y: 6.0,
+    z: 0.0,
+};
+const N: Vec3 = Vec3 {
+    x: 2.0,
+    y: 0.0,
+    z: 0.0,
+};
+
+#[test]
+fn point_in_triangle_2D() {
+    assert!(is_point_in_triangle_2D(A, B, C, D));
+    assert!(!is_point_in_triangle_2D(A, B, C, E));
+    assert!(is_point_in_triangle_2D(A, B, C, F));
+    assert!(is_point_in_triangle_2D(A, B, C, G));
+    assert!(is_point_in_triangle_2D(A, B, C, H));
+    assert!(!is_point_in_triangle_2D(A, B, C, I));
+    assert!(!is_point_in_triangle_2D(A, B, C, J));
+    assert!(!is_point_in_triangle_2D(A, B, C, K));
+    assert!(!is_point_in_triangle_2D(A, B, C, L));
+    assert!(!is_point_in_triangle_2D(A, B, C, M));
+    assert!(!is_point_in_triangle_2D(A, B, C, N));
+    assert!(is_point_in_triangle_2D(A, B, C, O));
+    assert!(is_point_in_triangle_2D(J, M, K, A));
+    assert!(is_point_in_triangle_2D(J, M, K, N));
+    assert!(is_point_in_triangle_2D(J, M, K, H));
+    assert!(!is_point_in_triangle_2D(J, M, K, F));
+    assert!(!is_point_in_triangle_2D(J, M, K, I));
+    assert!(!is_point_in_triangle_2D(J, M, K, D));
+}
