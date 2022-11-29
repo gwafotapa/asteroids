7aee64a1431326cf3561e434186ae7d5691aa3b1
Guillaume <gwafotapa@gmail.com>
2022-11-29 16:28:46 +0100

    Refactor compass.rs

--- src/compass.rs
+++ src/compass.rs
@@ -1,23 +1,39 @@
 use bevy::{prelude::*, render::mesh::PrimitiveTopology};
 use rand::Rng;
 
-use crate::{boss::BOSS_Z, spaceship::Spaceship, CAMERA_Z, WINDOW_HEIGHT, WINDOW_WIDTH};
+use crate::{boss::BOSS_Z, spaceship::Spaceship, WINDOW_HEIGHT, WINDOW_WIDTH};
 
 const COMPASS_POSITION: Vec3 = Vec3 {
-    x: WINDOW_WIDTH / 2.0 - 10.0,
+    x: WINDOW_WIDTH / 2.0 - 150.0,
     y: WINDOW_HEIGHT / 2.0 - 10.0,
-    z: -CAMERA_Z,
+    z: 0.0,
+    // z: -CAMERA_Z,
+};
+// Needle position relative to the compass position
+const NEEDLE_POSITION: Vec3 = Vec3 {
+    x: 120.0,
+    y: 0.0,
+    z: 0.0,
 };
 const DISTANCE_TO_TARGET: f32 = 5000.0;
+const FONT: &'static str = "fonts/FiraSans-Bold.ttf";
+const FONT_SIZE: f32 = 20.0;
+const COLOR: Color = Color::DARK_GRAY;
+const NEEDLE_SCALE: f32 = 0.13;
+
+// #[derive(Component)]
+// pub struct Level {
+//     target: Vec3,
+//     // boss_spawned: bool,
+// }
 
 #[derive(Component)]
-pub struct Level {
+pub struct Compass {
     target: Vec3,
-    // boss_spawned: bool,
 }
 
 #[derive(Component)]
-pub struct Compass;
+pub struct Needle;
 
 pub fn setup(
     mut commands: Commands,
@@ -26,80 +42,72 @@ pub fn setup(
     asset_server: Res<AssetServer>,
     query: Query<&Transform, With<Camera>>,
 ) {
+    let font = asset_server.load(FONT);
+    let text_style = TextStyle {
+        font,
+        font_size: FONT_SIZE,
+        color: COLOR,
+    };
+    let text_alignment = TextAlignment::CENTER_LEFT;
+
     let mut rng = rand::thread_rng();
+    let camera = query.single();
     let x = rng.gen_range(-DISTANCE_TO_TARGET..DISTANCE_TO_TARGET);
     let y_max = (DISTANCE_TO_TARGET.powi(2) - x.powi(2)).sqrt();
     let y = rng.gen_range(-y_max..y_max);
 
-    commands
-        .spawn(
-            // Create a TextBundle that has a Text with a single section.
-            TextBundle::from_section(
-                // Accepts a `String` or any type that converts into a `String`, such as `&str`
-                // format!("Distance: {:12}", INITIAL_DISTANCE),
-                "",
-                TextStyle {
-                    font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                    font_size: 20.0,
-                    color: Color::GRAY,
-                },
-            )
-            // Set the alignment of the Text
-            // .with_text_alignment(TextAlignment::TOP_RIGHT)
-            // Set the style of the TextBundle itself.
-            .with_style(Style {
-                // align_self: AlignSelf::FlexStart,
-                position_type: PositionType::Absolute,
-                position: UiRect {
-                    top: Val::Px(0.0),
-                    left: Val::Px(WINDOW_WIDTH - 150.0),
-                    // bottom: Val::Px(10.0),
-                    // right: Val::Px(1000.0),
-                    ..default()
-                },
-                ..default()
-            }),
-        )
-        .insert(Level {
-            target: Vec3::new(x, y, BOSS_Z),
-            // boss_spawned: false,
-        });
-
-    let camera = query.single();
     let c1 = Vec3::new(75.0, 0.0, 0.0);
     let c2 = Vec3::new(-50.0, 50.0, 0.0);
     let c3 = Vec3::new(-25.0, 0.0, 0.0);
     let c4 = Vec3::new(-50.0, -50.0, 0.0);
     let v_pos = vec![c1, c2, c3, c3, c4, c1];
-    let mut compass = Mesh::new(PrimitiveTopology::TriangleList);
-    compass.insert_attribute(Mesh::ATTRIBUTE_POSITION, v_pos);
-    commands.spawn(Compass).insert(ColorMesh2dBundle {
-        mesh: meshes.add(compass).into(),
-        transform: Transform::from_translation(camera.translation + COMPASS_POSITION)
-            .with_scale(Vec3::splat(0.13)),
-        material: materials.add(Color::WHITE.into()),
-        ..default()
-    });
+    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, v_pos);
+
+    let compass = commands
+        .spawn(Compass {
+            target: Vec3::new(x, y, BOSS_Z),
+            // boss_spawned: false,
+        })
+        .insert(Text2dBundle {
+            text: Text::from_section("", text_style).with_alignment(text_alignment),
+            transform: Transform::from_translation(camera.translation + COMPASS_POSITION),
+            ..default()
+        })
+        .id();
+
+    let needle = commands
+        .spawn(Needle)
+        .insert(ColorMesh2dBundle {
+            mesh: meshes.add(mesh).into(),
+            transform: Transform::from_translation(NEEDLE_POSITION)
+                .with_scale(Vec3::splat(NEEDLE_SCALE)),
+            material: materials.add(COLOR.into()),
+            ..default()
+        })
+        .id();
+
+    commands.entity(compass).add_child(needle);
 }
 
 pub fn update(
-    mut query_level: Query<(&Level, &mut Text)>,
-    query_spaceship: Query<&Transform, With<Spaceship>>,
-    query_camera: Query<&Transform, With<Camera>>,
-    mut query_compass: Query<&mut Transform, (With<Compass>, Without<Spaceship>, Without<Camera>)>,
+    mut query_compass: Query<(&Compass, &mut Transform, &mut Text)>,
+    mut query_needle: Query<&mut Transform, (With<Needle>, Without<Compass>)>,
+    query_spaceship: Query<&Transform, (With<Spaceship>, Without<Compass>, Without<Needle>)>,
+    query_camera: Query<&Transform, (With<Camera>, Without<Compass>, Without<Needle>)>,
 ) {
     if let Ok(spaceship) = query_spaceship.get_single() {
-        let (level, mut text) = query_level.single_mut();
         let camera = query_camera.single();
-        let mut compass = query_compass.single_mut();
-        compass.translation = camera.translation + COMPASS_POSITION;
-        compass.rotation = Quat::from_rotation_arc_2d(
+        let (compass, mut compass_transform, mut text) = query_compass.single_mut();
+        let mut needle = query_needle.single_mut();
+        compass_transform.translation = camera.translation + COMPASS_POSITION;
+        needle.rotation = Quat::from_rotation_arc_2d(
             Vec2::X,
-            (level.target - spaceship.translation)
+            (compass.target - spaceship.translation)
                 .truncate()
                 .normalize(),
         );
-        let distance = (level.target - spaceship.translation).length();
-        text.sections[0].value = format!("Target: {:>7.0}", distance);
+        let distance = (compass.target - spaceship.translation).length();
+        text.sections[0].value = format!("Target: {:<7.0}", distance);
     }
 }
