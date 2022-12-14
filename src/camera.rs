fdf53a5e614a2c36ad6b07beaddf9a371a95d736
Guillaume <gwafotapa@gmail.com>
2022-12-14 12:14:17 +0100

    In Ahead position, camera now follows the direction of the ship instead of its velocity

--- src/camera.rs
+++ src/camera.rs
@@ -6,13 +6,13 @@ use crate::{
 };
 
 const CAMERA_Z: f32 = 1000.0;
-const CAMERA_SPEED: f32 = 0.05;
+const CAMERA_SPEED: f32 = 0.02;
 const CAMERA_REAR_GAP: f32 = 100.0;
 
 #[derive(Component, Eq, PartialEq)]
 pub enum CameraPositioning {
-    Center,
-    Rear,
+    Synchronized,
+    Ahead,
 }
 
 pub fn spawn(mut commands: Commands) {
@@ -23,7 +23,7 @@ pub fn spawn(mut commands: Commands) {
             transform: Transform::from_xyz(WINDOW_WIDTH / 2.0, WINDOW_HEIGHT / 2.0, CAMERA_Z),
             ..default()
         })
-        .insert(CameraPositioning::Center)
+        .insert(CameraPositioning::Synchronized)
         .insert(UiCameraConfig { show_ui: true });
 }
 
@@ -37,31 +37,27 @@ pub fn update(
         c_transform.translation += s_velocity.0;
 
         if keys.any_just_pressed([KeyCode::Space, KeyCode::C]) {
-            *camera_positioning = if *camera_positioning == CameraPositioning::Center {
-                CameraPositioning::Rear
-            } else {
-                CameraPositioning::Center
+            *camera_positioning = match *camera_positioning {
+                CameraPositioning::Synchronized => CameraPositioning::Ahead,
+                CameraPositioning::Ahead => CameraPositioning::Synchronized,
             };
         }
 
-        if *camera_positioning == CameraPositioning::Rear {
-            // In Rear position, the camera places itself so that the ship is at distance 100.0
-            // from the window border with the velocity vector of the ship (not the ship itself)
-            // pointing at the camera (which is always at the center of the window).
-            // The window is a rectangle of dimensions WINDOW_WIDTH and WINDOW_HEIGHT.
+        if *camera_positioning == CameraPositioning::Ahead {
+            // In that position, the camera moves to position itself so that the ship
+            // is at distance 100.0 from the window border facing the center of the window.
             // Consider the inside rectangle obtained by removing a 100-width strip
-            // from the window. We aim to place the camera on this rectangle.
+            // from the window. We aim to place the ship on this rectangle.
             // The diagonals of this rectangle split its area into 4 quadrants.
-            // The computation depends on which quadrant the destination of the camera is.
+            // The computation depends on which quadrant the camera needs to move into.
             let c_destination;
-            if s_velocity.0 == Vec3::ZERO {
-                c_destination = s_transform.translation;
-            } else if s_velocity.0.x == 0.0
-                || (s_velocity.0.y / s_velocity.0.x).abs()
+            let direction = s_transform.rotation * Vec3::X;
+            if direction.x == 0.0
+                || (direction.y / direction.x).abs()
                     > (WINDOW_HEIGHT / 2.0 - CAMERA_REAR_GAP)
                         / (WINDOW_WIDTH / 2.0 - CAMERA_REAR_GAP)
             {
-                let y = if s_velocity.0.y > 0.0 {
+                let y = if direction.y > 0.0 {
                     // Upper quadrant
                     WINDOW_HEIGHT / 2.0 - CAMERA_REAR_GAP
                 } else {
@@ -70,23 +66,23 @@ pub fn update(
                 };
                 c_destination = s_transform.translation
                     + Vec3 {
-                        x: y * s_velocity.0.x / s_velocity.0.y,
+                        x: y * direction.x / direction.y,
                         y,
                         z: CAMERA_Z - SPACESHIP_Z,
                     };
             } else {
-                let x = if s_velocity.0.x > 0.0 {
+                let x = if direction.x > 0.0 {
                     // Right quadrant
                     WINDOW_WIDTH / 2.0 - CAMERA_REAR_GAP
                 } else {
-                    // Lower quadrant
+                    // Left quadrant
                     -(WINDOW_WIDTH / 2.0 - CAMERA_REAR_GAP)
                 };
 
                 c_destination = s_transform.translation
                     + Vec3 {
                         x,
-                        y: s_velocity.0.y / s_velocity.0.x * x,
+                        y: direction.y / direction.x * x,
                         z: CAMERA_Z - SPACESHIP_Z,
                     };
             }
@@ -101,5 +97,47 @@ pub fn update(
             };
             c_transform.translation += CAMERA_SPEED * direction;
         }
+        // } else {
+        //     let c_destination;
+        //     if s_velocity.0 == Vec3::ZERO {
+        //         c_destination = s_transform.translation;
+        //     } else if s_velocity.0.x == 0.0
+        //         || (s_velocity.0.y / s_velocity.0.x).abs()
+        //             > (WINDOW_HEIGHT / 2.0 - CAMERA_REAR_GAP)
+        //                 / (WINDOW_WIDTH / 2.0 - CAMERA_REAR_GAP)
+        //     {
+        //         let y = if s_velocity.0.y > 0.0 {
+        //             // Upper quadrant
+        //             WINDOW_HEIGHT / 2.0 - CAMERA_REAR_GAP
+        //         } else {
+        //             // Lower quadrant
+        //             -(WINDOW_HEIGHT / 2.0 - CAMERA_REAR_GAP)
+        //         };
+        //         c_destination = s_transform.translation
+        //             + Vec3 {
+        //                 x: y * s_velocity.0.x / s_velocity.0.y,
+        //                 y,
+        //                 z: CAMERA_Z - SPACESHIP_Z,
+        //             };
+        //     } else {
+        //         let x = if s_velocity.0.x > 0.0 {
+        //             // Right quadrant
+        //             WINDOW_WIDTH / 2.0 - CAMERA_REAR_GAP
+        //         } else {
+        //             // Left quadrant
+        //             -(WINDOW_WIDTH / 2.0 - CAMERA_REAR_GAP)
+        //         };
+
+        //         c_destination = s_transform.translation
+        //             + Vec3 {
+        //                 x,
+        //                 y: s_velocity.0.y / s_velocity.0.x * x,
+        //                 z: CAMERA_Z - SPACESHIP_Z,
+        //             };
+        //     }
+
+        //     let c_path = c_destination - c_transform.translation;
+        //     c_transform.translation += CAMERA_SPEED * c_path;
+        // }
     }
 }
