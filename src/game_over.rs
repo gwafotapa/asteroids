ab05e243f10fb3dc344fd088ded886db21b8c977
Guillaume <gwafotapa@gmail.com>
2022-12-15 20:21:47 +0100

    Add a game_over module

---
+++ src/game_over.rs
@@ -0,0 +1,54 @@
+use bevy::prelude::*;
+use iyes_loopless::prelude::*;
+
+use super::GameState;
+
+#[derive(Clone, Component, Copy)]
+pub struct GameOver;
+
+pub fn spawn_text(
+    query_spaceship: Query<&super::Health, With<super::spaceship::Spaceship>>,
+    mut query_camera: Query<&mut UiCameraConfig>,
+    mut query_pause_menu: Query<&mut Style, With<super::ui::pause_menu::PauseMenu>>,
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    if let Ok(health) = query_spaceship.get_single() {
+        if health.0 == 0 {
+            commands.spawn(GameOver).insert(TextBundle {
+                text: Text::from_section(
+                    "Press space to go back to the main menu",
+                    TextStyle {
+                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                        font_size: 24.0,
+                        color: Color::rgb(0.0, 0.0, 0.0),
+                    },
+                ),
+                style: Style {
+                    align_self: AlignSelf::Center,
+                    margin: UiRect::all(Val::Auto),
+                    ..Default::default()
+                },
+                ..Default::default()
+            });
+
+            query_pause_menu.single_mut().display = Display::None;
+            query_camera.single_mut().show_ui = true;
+        }
+    }
+}
+
+pub fn update_text(
+    mut query: Query<&mut Text, With<GameOver>>,
+    input: Res<Input<KeyCode>>,
+    mut commands: Commands,
+) {
+    if let Ok(mut text) = query.get_single_mut() {
+        if text.sections[0].style.color.r() < 0.5 {
+            text.sections[0].style.color += Color::rgb(0.002, 0.002, 0.002);
+        }
+        if input.just_pressed(KeyCode::Space) {
+            commands.insert_resource(NextState(GameState::TurnDownLight));
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
 #![allow(clippy::type_complexity)]
-use bevy::{
-    input::{keyboard::KeyboardInput, ButtonState},
-    prelude::*,
-};
+use bevy::prelude::*;
 use iyes_loopless::prelude::*;
 
 use crate::collision::math::{triangle::TriangleXY, Collider, Topology};
@@ -14,6 +11,7 @@ pub mod camera;
 pub mod collision;
 pub mod compass;
 pub mod fire;
+pub mod game_over;
 pub mod light;
 pub mod map;
 pub mod spaceship;
@@ -65,56 +63,6 @@ pub fn exit_game_setup(mut commands: Commands) {
 //     }
 //     // query_camera.single_mut().show_ui = true;
 // }
-
-#[derive(Clone, Component, Copy)]
-pub struct GameOver;
-
-pub fn game_over_spawn_text(
-    query_spaceship: Query<&Health, With<spaceship::Spaceship>>,
-    mut query_camera: Query<&mut UiCameraConfig>,
-    mut query_pause_menu: Query<&mut Style, With<ui::pause_menu::PauseMenu>>,
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-) {
-    if let Ok(health) = query_spaceship.get_single() {
-        if health.0 == 0 {
-            commands.spawn(GameOver).insert(TextBundle {
-                text: Text::from_section(
-                    "Press space to go back to the main menu",
-                    TextStyle {
-                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                        font_size: 24.0,
-                        color: Color::rgb(0.0, 0.0, 0.0),
-                    },
-                ),
-                style: Style {
-                    align_self: AlignSelf::Center,
-                    margin: UiRect::all(Val::Auto),
-                    ..Default::default()
-                },
-                ..Default::default()
-            });
-
-            query_pause_menu.single_mut().display = Display::None;
-            query_camera.single_mut().show_ui = true;
-        }
-    }
-}
-
-pub fn game_over_update_text(
-    mut query: Query<&mut Text, With<GameOver>>,
-    input: Res<Input<KeyCode>>,
-    mut commands: Commands,
-) {
-    if let Ok(mut text) = query.get_single_mut() {
-        if text.sections[0].style.color.r() < 0.5 {
-            text.sections[0].style.color += Color::rgb(0.002, 0.002, 0.002);
-        }
-        if input.just_pressed(KeyCode::Space) {
-            commands.insert_resource(NextState(GameState::TurnDownLight));
-        }
-    }
-}
 
 pub fn spaceship_exists(query: Query<With<spaceship::Spaceship>>) -> bool {
     !query.is_empty()
