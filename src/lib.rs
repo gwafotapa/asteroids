d44851e9d7e3a146aa4fd34ca54395d5d483e1ae
Guillaume <gwafotapa@gmail.com>
2022-12-15 20:15:58 +0100

    Change game over wrap up

--- src/lib.rs
+++ src/lib.rs
@@ -44,7 +44,7 @@ pub enum GameState {
     GameSetup,
     InGame,
     Paused,
-    // GameOver,
+    GameOver,
     TurnDownLight,
     TurnUpLight,
 }
@@ -66,17 +66,53 @@ pub fn exit_game_setup(mut commands: Commands) {
 //     // query_camera.single_mut().show_ui = true;
 // }
 
-pub fn game_over(
-    query: Query<With<spaceship::Spaceship>>,
-    mut keyboard_activity: EventReader<KeyboardInput>,
+#[derive(Clone, Component, Copy)]
+pub struct GameOver;
+
+pub fn game_over_spawn_text(
+    query_spaceship: Query<&Health, With<spaceship::Spaceship>>,
+    mut query_camera: Query<&mut UiCameraConfig>,
+    mut query_pause_menu: Query<&mut Style, With<ui::pause_menu::PauseMenu>>,
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    if let Ok(health) = query_spaceship.get_single() {
+        if health.0 == 0 {
+            commands.spawn(GameOver).insert(TextBundle {
+                text: Text::from_section(
+                    "Press space to go back to the main menu",
+                    TextStyle {
+                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                        font_size: 24.0,
+                        color: Color::rgb(0.0, 0.0, 0.0),
+                    },
+                ),
+                style: Style {
+                    align_self: AlignSelf::Center,
+                    margin: UiRect::all(Val::Auto),
+                    ..Default::default()
+                },
+                ..Default::default()
+            });
+
+            query_pause_menu.single_mut().display = Display::None;
+            query_camera.single_mut().show_ui = true;
+        }
+    }
+}
+
+pub fn game_over_update_text(
+    mut query: Query<&mut Text, With<GameOver>>,
+    input: Res<Input<KeyCode>>,
     mut commands: Commands,
 ) {
-    if query.get_single().is_err()
-        && keyboard_activity
-            .iter()
-            .any(|key| key.state == ButtonState::Pressed)
-    {
-        commands.insert_resource(NextState(GameState::TurnDownLight))
+    if let Ok(mut text) = query.get_single_mut() {
+        if text.sections[0].style.color.r() < 0.5 {
+            text.sections[0].style.color += Color::rgb(0.002, 0.002, 0.002);
+        }
+        if input.just_pressed(KeyCode::Space) {
+            commands.insert_resource(NextState(GameState::TurnDownLight));
+        }
     }
 }
 
