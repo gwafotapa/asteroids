2fd7049cf78b99b4982425bb1d46d46d5ecc79eb
Guillaume <gwafotapa@gmail.com>
2023-01-13 11:52:29 +0100

    Remove an unnecessary Option

--- src/collision/generic.rs
+++ src/collision/generic.rs
@@ -18,7 +18,7 @@ pub fn with<C: Component>(
     mut query_c: Query<
         (
             &mut AngularVelocity,
-            Option<&Children>,
+            &Children,
             &Mass,
             &MomentOfInertia,
             &mut Transform,
@@ -44,14 +44,14 @@ pub fn with<C: Component>(
     'outer: while let Some(
         [(
             mut angular_velocity1,
-            maybe_children1,
+            children1,
             mass1,
             moment_of_inertia1,
             mut transform1,
             mut velocity1,
         ), (
             mut angular_velocity2,
-            maybe_children2,
+            children2,
             mass2,
             moment_of_inertia2,
             mut transform2,
@@ -59,87 +59,85 @@ pub fn with<C: Component>(
         )],
     ) = combinations.fetch_next()
     {
-        if let Some((children1, children2)) = maybe_children1.zip(maybe_children2) {
-            let mut time_c = time.delta_seconds();
-            if let Some((contact, entity1p, entity2p)) = detection::intersection_at(
-                &mut transform1,
-                &mut transform2,
-                &mut time_c,
+        let mut time_c = time.delta_seconds();
+        if let Some((contact, entity1p, entity2p)) = detection::intersection_at(
+            &mut transform1,
+            &mut transform2,
+            &mut time_c,
+            *mass1,
+            *mass2,
+            *moment_of_inertia1,
+            *moment_of_inertia2,
+            *velocity1,
+            *velocity2,
+            *angular_velocity1,
+            *angular_velocity2,
+            &query_c_part,
+            &query_c_part,
+            children1,
+            children2,
+            Res::clone(&meshes),
+        ) {
+            // if !cache.contains(Collision(spaceship, b_id)) {
+            response::compute_velocities(
+                &mut velocity1,
+                &mut velocity2,
+                &mut angular_velocity1,
+                &mut angular_velocity2,
+                *transform1,
+                *transform2,
                 *mass1,
                 *mass2,
                 *moment_of_inertia1,
                 *moment_of_inertia2,
+                contact,
+            );
+
+            damages::apply(
+                query_c_part_mut.get_many_mut([entity1p, entity2p]).unwrap(),
+                *mass1,
+                *mass2,
                 *velocity1,
                 *velocity2,
-                *angular_velocity1,
-                *angular_velocity2,
-                &query_c_part,
-                &query_c_part,
-                children1,
-                children2,
-                Res::clone(&meshes),
-            ) {
-                // if !cache.contains(Collision(spaceship, b_id)) {
-                response::compute_velocities(
-                    &mut velocity1,
-                    &mut velocity2,
-                    &mut angular_velocity1,
-                    &mut angular_velocity2,
+                materials.as_mut(),
+            );
+
+            if query_c_part_mut
+                .get_component::<Health>(entity1p)
+                .unwrap()
+                .0
+                > 0
+            {
+                *transform1 = transform::at(
+                    time.delta_seconds() - time_c,
                     *transform1,
-                    *transform2,
-                    *mass1,
-                    *mass2,
-                    *moment_of_inertia1,
-                    *moment_of_inertia2,
-                    contact,
-                );
-
-                damages::apply(
-                    query_c_part_mut.get_many_mut([entity1p, entity2p]).unwrap(),
-                    *mass1,
-                    *mass2,
                     *velocity1,
-                    *velocity2,
-                    materials.as_mut(),
+                    *angular_velocity1,
                 );
-
-                if query_c_part_mut
-                    .get_component::<Health>(entity1p)
-                    .unwrap()
-                    .0
-                    > 0
-                {
-                    *transform1 = transform::at(
-                        time.delta_seconds() - time_c,
-                        *transform1,
-                        *velocity1,
-                        *angular_velocity1,
-                    );
-                }
-
-                if query_c_part_mut
-                    .get_component::<Health>(entity2p)
-                    .unwrap()
-                    .0
-                    > 0
-                {
-                    *transform2 = transform::at(
-                        time.delta_seconds() - time_c,
-                        *transform2,
-                        *velocity2,
-                        *angular_velocity2,
-                    );
-                }
-
-                debug!(
-                    "translation1_c = {}, translation2_c = {}\n\
-		     velocity1 = {}, velocity2 = {}\n",
-                    transform1.translation, transform2.translation, velocity1.0, velocity2.0,
-                );
-
-                // cache.add(Collision(spaceship, b_id));
-                continue 'outer;
             }
+
+            if query_c_part_mut
+                .get_component::<Health>(entity2p)
+                .unwrap()
+                .0
+                > 0
+            {
+                *transform2 = transform::at(
+                    time.delta_seconds() - time_c,
+                    *transform2,
+                    *velocity2,
+                    *angular_velocity2,
+                );
+            }
+
+            debug!(
+                "translation1_c = {}, translation2_c = {}\n\
+		     velocity1 = {}, velocity2 = {}\n",
+                transform1.translation, transform2.translation, velocity1.0, velocity2.0,
+            );
+
+            // cache.add(Collision(spaceship, b_id));
+            continue 'outer;
         }
     }
 }
@@ -149,7 +147,7 @@ pub fn between<C1: Component, C2: Component>(
     mut query_c1: Query<
         (
             &mut AngularVelocity,
-            Option<&Children>,
+            &Children,
             &Mass,
             &MomentOfInertia,
             &mut Transform,
@@ -160,7 +158,7 @@ pub fn between<C1: Component, C2: Component>(
     mut query_c2: Query<
         (
             &mut AngularVelocity,
-            Option<&Children>,
+            &Children,
             &Mass,
             &MomentOfInertia,
             &mut Transform,
@@ -194,111 +192,104 @@ pub fn between<C1: Component, C2: Component>(
 ) {
     'outer: for (
         mut angular_velocity1,
-        maybe_children1,
+        children1,
         mass1,
         moment_of_inertia1,
         mut transform1,
         mut velocity1,
     ) in query_c1.iter_mut()
     {
-        if let Some(children1) = maybe_children1 {
-            for (
-                mut angular_velocity2,
-                maybe_children2,
-                mass2,
-                moment_of_inertia2,
-                mut transform2,
-                mut velocity2,
-            ) in query_c2.iter_mut()
-            {
-                if let Some(children2) = maybe_children2 {
-                    let mut time_c = time.delta_seconds();
-                    if let Some((contact, entity1p, entity2p)) = detection::intersection_at(
-                        &mut transform1,
-                        &mut transform2,
-                        &mut time_c,
-                        *mass1,
-                        *mass2,
-                        *moment_of_inertia1,
-                        *moment_of_inertia2,
+        for (
+            mut angular_velocity2,
+            children2,
+            mass2,
+            moment_of_inertia2,
+            mut transform2,
+            mut velocity2,
+        ) in query_c2.iter_mut()
+        {
+            let mut time_c = time.delta_seconds();
+            if let Some((contact, entity1p, entity2p)) = detection::intersection_at(
+                &mut transform1,
+                &mut transform2,
+                &mut time_c,
+                *mass1,
+                *mass2,
+                *moment_of_inertia1,
+                *moment_of_inertia2,
+                *velocity1,
+                *velocity2,
+                *angular_velocity1,
+                *angular_velocity2,
+                &query_c1_part,
+                &query_c2_part,
+                children1,
+                children2,
+                Res::clone(&meshes),
+            ) {
+                // if !cache.contains(Collision(spaceship, b_id)) {
+                response::compute_velocities(
+                    &mut velocity1,
+                    &mut velocity2,
+                    &mut angular_velocity1,
+                    &mut angular_velocity2,
+                    *transform1,
+                    *transform2,
+                    *mass1,
+                    *mass2,
+                    *moment_of_inertia1,
+                    *moment_of_inertia2,
+                    contact,
+                );
+
+                damages::apply(
+                    [
+                        query_c1_part_mut.get_mut(entity1p).unwrap(),
+                        query_c2_part_mut.get_mut(entity2p).unwrap(),
+                    ],
+                    *mass1,
+                    *mass2,
+                    *velocity1,
+                    *velocity2,
+                    materials.as_mut(),
+                );
+
+                if query_c1_part_mut
+                    .get_component::<Health>(entity1p)
+                    .unwrap()
+                    .0
+                    > 0
+                {
+                    *transform1 = transform::at(
+                        time.delta_seconds() - time_c,
+                        *transform1,
                         *velocity1,
-                        *velocity2,
                         *angular_velocity1,
-                        *angular_velocity2,
-                        &query_c1_part,
-                        &query_c2_part,
-                        children1,
-                        children2,
-                        Res::clone(&meshes),
-                    ) {
-                        // if !cache.contains(Collision(spaceship, b_id)) {
-                        response::compute_velocities(
-                            &mut velocity1,
-                            &mut velocity2,
-                            &mut angular_velocity1,
-                            &mut angular_velocity2,
-                            *transform1,
-                            *transform2,
-                            *mass1,
-                            *mass2,
-                            *moment_of_inertia1,
-                            *moment_of_inertia2,
-                            contact,
-                        );
-
-                        damages::apply(
-                            [
-                                query_c1_part_mut.get_mut(entity1p).unwrap(),
-                                query_c2_part_mut.get_mut(entity2p).unwrap(),
-                            ],
-                            *mass1,
-                            *mass2,
-                            *velocity1,
-                            *velocity2,
-                            materials.as_mut(),
-                        );
-
-                        if query_c1_part_mut
-                            .get_component::<Health>(entity1p)
-                            .unwrap()
-                            .0
-                            > 0
-                        {
-                            *transform1 = transform::at(
-                                time.delta_seconds() - time_c,
-                                *transform1,
-                                *velocity1,
-                                *angular_velocity1,
-                            );
-                        }
-
-                        if query_c2_part_mut
-                            .get_component::<Health>(entity2p)
-                            .unwrap()
-                            .0
-                            > 0
-                        {
-                            *transform2 = transform::at(
-                                time.delta_seconds() - time_c,
-                                *transform2,
-                                *velocity2,
-                                *angular_velocity2,
-                            );
-                        }
-
-                        debug!(
-                            "translation1_c = {}, translation2_c = {}\n\
-			     velocity1 = {}, velocity2 = {}\n",
-                            transform1.translation,
-                            transform2.translation,
-                            velocity1.0,
-                            velocity2.0,
-                        );
-
-                        // cache.add(Collision(spaceship, b_id));
-                        continue 'outer;
-                    }
+                    );
                 }
+
+                if query_c2_part_mut
+                    .get_component::<Health>(entity2p)
+                    .unwrap()
+                    .0
+                    > 0
+                {
+                    *transform2 = transform::at(
+                        time.delta_seconds() - time_c,
+                        *transform2,
+                        *velocity2,
+                        *angular_velocity2,
+                    );
+                }
+
+                debug!(
+                    "translation1_c = {}, translation2_c = {}\n\
+			     velocity1 = {}, velocity2 = {}\n",
+                    transform1.translation, transform2.translation, velocity1.0, velocity2.0,
+                );
+
+                // cache.add(Collision(spaceship, b_id));
+                continue 'outer;
             }
         }
     }
@@ -309,7 +300,7 @@ pub fn among<C1: Component, C2: Component>(
     mut query: Query<
         (
             &mut AngularVelocity,
-            Option<&Children>,
+            &Children,
             &Mass,
             &MomentOfInertia,
             &mut Transform,
@@ -335,14 +326,14 @@ pub fn among<C1: Component, C2: Component>(
     'outer: while let Some(
         [(
             mut angular_velocity1,
-            maybe_children1,
+            children1,
             mass1,
             moment_of_inertia1,
             mut transform1,
             mut velocity1,
         ), (
             mut angular_velocity2,
-            maybe_children2,
+            children2,
             mass2,
             moment_of_inertia2,
             mut transform2,
@@ -350,77 +341,75 @@ pub fn among<C1: Component, C2: Component>(
         )],
     ) = combinations.fetch_next()
     {
-        if let Some((children1, children2)) = maybe_children1.zip(maybe_children2) {
-            let mut time_c = time.delta_seconds();
-            if let Some((contact, entity1p, entity2p)) = detection::intersection_at(
-                &mut transform1,
-                &mut transform2,
-                &mut time_c,
+        let mut time_c = time.delta_seconds();
+        if let Some((contact, entity1p, entity2p)) = detection::intersection_at(
+            &mut transform1,
+            &mut transform2,
+            &mut time_c,
+            *mass1,
+            *mass2,
+            *moment_of_inertia1,
+            *moment_of_inertia2,
+            *velocity1,
+            *velocity2,
+            *angular_velocity1,
+            *angular_velocity2,
+            &query_part,
+            &query_part,
+            children1,
+            children2,
+            Res::clone(&meshes),
+        ) {
+            // if !cache.contains(Collision(spaceship, b_id)) {
+            response::compute_velocities(
+                &mut velocity1,
+                &mut velocity2,
+                &mut angular_velocity1,
+                &mut angular_velocity2,
+                *transform1,
+                *transform2,
                 *mass1,
                 *mass2,
                 *moment_of_inertia1,
                 *moment_of_inertia2,
+                contact,
+            );
+
+            damages::apply(
+                query_part_mut.get_many_mut([entity1p, entity2p]).unwrap(),
+                *mass1,
+                *mass2,
                 *velocity1,
                 *velocity2,
-                *angular_velocity1,
-                *angular_velocity2,
-                &query_part,
-                &query_part,
-                children1,
-                children2,
-                Res::clone(&meshes),
-            ) {
-                // if !cache.contains(Collision(spaceship, b_id)) {
-                response::compute_velocities(
-                    &mut velocity1,
-                    &mut velocity2,
-                    &mut angular_velocity1,
-                    &mut angular_velocity2,
+                materials.as_mut(),
+            );
+
+            if query_part_mut.get_component::<Health>(entity1p).unwrap().0 > 0 {
+                *transform1 = transform::at(
+                    time.delta_seconds() - time_c,
                     *transform1,
-                    *transform2,
-                    *mass1,
-                    *mass2,
-                    *moment_of_inertia1,
-                    *moment_of_inertia2,
-                    contact,
-                );
-
-                damages::apply(
-                    query_part_mut.get_many_mut([entity1p, entity2p]).unwrap(),
-                    *mass1,
-                    *mass2,
                     *velocity1,
-                    *velocity2,
-                    materials.as_mut(),
+                    *angular_velocity1,
                 );
-
-                if query_part_mut.get_component::<Health>(entity1p).unwrap().0 > 0 {
-                    *transform1 = transform::at(
-                        time.delta_seconds() - time_c,
-                        *transform1,
-                        *velocity1,
-                        *angular_velocity1,
-                    );
-                }
-
-                if query_part_mut.get_component::<Health>(entity2p).unwrap().0 > 0 {
-                    *transform2 = transform::at(
-                        time.delta_seconds() - time_c,
-                        *transform2,
-                        *velocity2,
-                        *angular_velocity2,
-                    );
-                }
-
-                debug!(
-                    "translation1_c = {}, translation2_c = {}\n\
-		     velocity1 = {}, velocity2 = {}\n",
-                    transform1.translation, transform2.translation, velocity1.0, velocity2.0,
-                );
-
-                // cache.add(Collision(spaceship, b_id));
-                continue 'outer;
             }
+
+            if query_part_mut.get_component::<Health>(entity2p).unwrap().0 > 0 {
+                *transform2 = transform::at(
+                    time.delta_seconds() - time_c,
+                    *transform2,
+                    *velocity2,
+                    *angular_velocity2,
+                );
+            }
+
+            debug!(
+                "translation1_c = {}, translation2_c = {}\n\
+		     velocity1 = {}, velocity2 = {}\n",
+                transform1.translation, transform2.translation, velocity1.0, velocity2.0,
+            );
+
+            // cache.add(Collision(spaceship, b_id));
+            continue 'outer;
         }
     }
 }
