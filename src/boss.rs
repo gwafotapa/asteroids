789685a115037715b867687fd61f51b64f190e3e
Guillaume <gwafotapa@gmail.com>
2023-01-19 15:09:46 +0100

    Clean module boss and start removing collision cache

--- src/boss.rs
+++ src/boss.rs
@@ -4,21 +4,15 @@ use std::f32::consts::{PI, SQRT_2};
 
 use crate::{
     blast::BlastEvent,
-    collision::{cache::Cache, detection::triangle::Triangle, Aabb, Collider, Topology},
+    collision::{detection::triangle::Triangle, Aabb, Collider, Topology},
     fire::{Fire, FireEvent},
     spaceship::{self, Spaceship},
     AngularVelocity, Health, Mass, MomentOfInertia, Part, Velocity, PLANE_Z,
 };
 
-pub const BOSS_Z: f32 = PLANE_Z;
-pub const DISTANCE_TO_BOSS: f32 = 10000.0;
 const INNER_RADIUS: f32 = 100.0;
 const OUTER_RADIUS: f32 = INNER_RADIUS * SQRT_2;
-const AREA: f32 = PI * (INNER_RADIUS + OUTER_RADIUS) / 2.0 * (INNER_RADIUS + OUTER_RADIUS) / 2.0;
-// const MASS: f32 = 20.0;
-const MASS: f32 = AREA;
-const MOMENT_OF_INERTIA: f32 =
-    0.5 * MASS * (INNER_RADIUS + OUTER_RADIUS) / 2.0 * (INNER_RADIUS + OUTER_RADIUS) / 2.0;
+const COLOR: Color = Color::rgb(0.25, 0.5, 0.25);
 
 #[derive(Component)]
 pub struct Boss;
@@ -38,125 +32,52 @@ pub struct Indestructible;
 #[derive(Component)]
 pub struct ColorDamaged(pub Color);
 
-// const A0: Vec3 = Vec3 {
-//     x: -OUTER_RADIUS,
-//     y: 0.0,
-//     z: 0.0,
-// };
+#[derive(Component)]
+pub struct Attack(pub Vec3);
+
 const A1: Vec3 = Vec3 {
     x: -INNER_RADIUS,
     y: INNER_RADIUS - OUTER_RADIUS,
     z: 0.0,
 };
-// const A2: Vec3 = Vec3 {
-//     x: -INNER_RADIUS,
-//     y: -INNER_RADIUS,
-//     z: 0.0,
-// };
 const A3: Vec3 = Vec3 {
     x: INNER_RADIUS - OUTER_RADIUS,
     y: -INNER_RADIUS,
     z: 0.0,
 };
-// const A4: Vec3 = Vec3 {
-//     x: 0.0,
-//     y: -OUTER_RADIUS,
-//     z: 0.0,
-// };
 const A5: Vec3 = Vec3 {
     x: OUTER_RADIUS - INNER_RADIUS,
     y: -INNER_RADIUS,
     z: 0.0,
 };
-// const A6: Vec3 = Vec3 {
-//     x: INNER_RADIUS,
-//     y: -INNER_RADIUS,
-//     z: 0.0,
-// };
 const A7: Vec3 = Vec3 {
     x: INNER_RADIUS,
     y: INNER_RADIUS - OUTER_RADIUS,
     z: 0.0,
 };
-// const A8: Vec3 = Vec3 {
-//     x: OUTER_RADIUS,
-//     y: 0.0,
-//     z: 0.0,
-// };
 const A9: Vec3 = Vec3 {
     x: INNER_RADIUS,
     y: OUTER_RADIUS - INNER_RADIUS,
     z: 0.0,
 };
-// const A10: Vec3 = Vec3 {
-//     x: INNER_RADIUS,
-//     y: INNER_RADIUS,
-//     z: 0.0,
-// };
 const A11: Vec3 = Vec3 {
     x: OUTER_RADIUS - INNER_RADIUS,
     y: INNER_RADIUS,
     z: 0.0,
 };
-// const A12: Vec3 = Vec3 {
-//     x: 0.0,
-//     y: OUTER_RADIUS,
-//     z: 0.0,
-// };
 const A13: Vec3 = Vec3 {
     x: INNER_RADIUS - OUTER_RADIUS,
     y: INNER_RADIUS,
     z: 0.0,
 };
-// const A14: Vec3 = Vec3 {
-//     x: -INNER_RADIUS,
-//     y: INNER_RADIUS,
-//     z: 0.0,
-// };
 const A15: Vec3 = Vec3 {
     x: -INNER_RADIUS,
     y: OUTER_RADIUS - INNER_RADIUS,
     z: 0.0,
 };
 
-// /// Counter clockwise
-// pub const POLYGON: [Vec3; 16] = [
-//     A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15,
-// ];
-
-// const ACCELERATION: f32 = 0.0;
-const ACCELERATION: f32 = 500.0;
-const ANGULAR_DRAG: f32 = 0.25;
-const DRAG: f32 = 0.05;
-const ATTACK_COLOR: Color = Color::RED;
-const BLAST_RADIUS: f32 = 15.0;
-const BLAST_VERTICES: usize = 32;
-const COLOR: Color = Color::rgb(0.25, 0.5, 0.25);
-const CORE_HEALTH: u32 = 50;
-const EDGE_HEALTH: u32 = 15;
-const FIRE_DAMAGES: u32 = 1;
-const FIRE_VELOCITY: f32 = 400.0;
-const FIRE_RADIUS: f32 = 5.0 / FIRE_RANGE as f32;
-const FIRE_RANGE: u32 = 100;
-const FIRE_VERTICES: usize = 32;
-const FIRE_IMPACT_RADIUS: f32 = 15.0;
-const FIRE_IMPACT_VERTICES: usize = 32;
-
-// const INITIAL_TRANSLATION: Vec3 = Vec3 {
-//     // x: WINDOW_WIDTH / 2.0 + OUTER_RADIUS,
-//     x: WINDOW_WIDTH / 2.0,
-//     y: 0.0,
-//     z: BOSS_Z,
-// };
-// const ROTATION_SPEED: f32 = 0.0;
-const ROTATION_SPEED: f32 = 20.0;
-
-#[derive(Component)]
-pub struct Attack(pub Vec3);
-
 // The body is a collection of 6 triangles. It is a single part of the boss.
-const CORE_PARTS: usize = 6;
-const CORE_TRIANGLES: [Triangle; CORE_PARTS] = [
+const CORE_TRIANGLES: [Triangle; 6] = [
     Triangle(A1, A3, A15),
     Triangle(A3, A13, A15),
     Triangle(A3, A11, A13),
@@ -167,7 +88,6 @@ const CORE_TRIANGLES: [Triangle; CORE_PARTS] = [
 
 // There are 8 egdes.
 // Each edge is a triangle and constitutes a whole part of the boss.
-const EDGES: usize = 8;
 const E1: Vec3 = Vec3 {
     x: OUTER_RADIUS - INNER_RADIUS,
     y: 0.0,
@@ -185,30 +105,23 @@ const E3: Vec3 = Vec3 {
 };
 const EDGE_TRIANGLES: [Triangle; 1] = [Triangle(E1, E2, E3)];
 
-// pub fn triangles_from_polygon(polygon: &[Vec3], center: Vec3) -> Vec<Vec3> {
-//     let mut triangles = Vec::new();
-//     for (&a, &b) in polygon
-//         .iter()
-//         .zip(polygon.iter().skip(1).chain(polygon.iter().take(1)))
-//     {
-//         triangles.extend_from_slice(&[center, a, b]);
-//     }
-//     triangles
-// }
-
 pub fn spawn(
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
-    // query: Query<&Compass>,
-    // query_asteroid: Query<With<Asteroid>>,
+    mut meshes: ResMut<Assets<Mesh>>,
 ) {
-    // Compute boss starting position
     let mut rng = rand::thread_rng();
     let theta = rng.gen_range(0.0..2.0 * PI);
+    const DISTANCE_TO_BOSS: f32 = 10000.0;
     let x = DISTANCE_TO_BOSS * theta.cos() + spaceship::TRANSLATION.x;
     let y = DISTANCE_TO_BOSS * theta.sin() + spaceship::TRANSLATION.y;
+    const BOSS_Z: f32 = PLANE_Z;
     let translation = Vec3::new(x, y, BOSS_Z);
+    const AREA: f32 =
+        PI * (INNER_RADIUS + OUTER_RADIUS) / 2.0 * (INNER_RADIUS + OUTER_RADIUS) / 2.0;
+    const MASS: f32 = AREA;
+    const MOMENT_OF_INERTIA: f32 =
+        0.5 * MASS * (INNER_RADIUS + OUTER_RADIUS) / 2.0 * (INNER_RADIUS + OUTER_RADIUS) / 2.0;
 
     let boss = commands
         .spawn(Boss)
@@ -221,9 +134,6 @@ pub fn spawn(
             ..Default::default()
         })
         .id();
-
-    // let translation = query.single().target;
-    // if !level.boss_spawned && level.distance_to_boss == 0 && query_asteroid.is_empty() {
 
     // Build core
     let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
@@ -238,14 +148,9 @@ pub fn spawn(
     // mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, vertices_normal);
     // mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, vertices_uv);
     let mesh_handle = meshes.add(mesh);
-
-    // println!(
-    //     "boss\narea: {}\nmass: {}\nmoment of inertia: {}\n",
-    //     AREA, MASS, MOMENT_OF_INERTIA
-    // );
+    const CORE_HEALTH: u32 = 50;
 
     let boss_core = commands
-        // .spawn(Boss)
         .spawn((Boss, Part))
         .insert(BossCore)
         .insert(Health(CORE_HEALTH))
@@ -269,6 +174,7 @@ pub fn spawn(
     commands.entity(boss).add_child(boss_core);
 
     // Add the edges
+    const EDGES: usize = 8;
     for i in 0..EDGES {
         let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
         let vertices_position: Vec<[f32; 3]> = EDGE_TRIANGLES
@@ -276,15 +182,14 @@ pub fn spawn(
             .flat_map(|triangle| triangle.to_array())
             .map(|vertex| vertex.to_array())
             .collect();
-
         // vec![E1.to_array(), E2.to_array(), E3.to_array()];
         // let vertices_normal = vec![[0.0, 0.0, 1.0]; 3];
         // let vertices_uv = vec![[0.0, 0.0]; 3];
-
         mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, vertices_position);
         // mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, vertices_normal);
         // mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, vertices_uv);
         let mesh_handle = meshes.add(mesh);
+        const EDGE_HEALTH: u32 = 15;
 
         let boss_edge = commands
             .spawn((Boss, Part))
@@ -319,47 +224,39 @@ pub fn spawn(
 
         commands.entity(boss).add_child(boss_edge);
     }
-
-    // level.boss_spawned = true;
-    // }
 }
 
 pub fn movement(
-    mut query_boss: Query<
-        (&mut AngularVelocity, Entity, &mut Transform, &mut Velocity),
-        With<Boss>,
-    >,
+    mut query_boss: Query<(&mut AngularVelocity, &mut Transform, &mut Velocity), With<Boss>>,
     query_boss_edge: Query<With<BossEdge>>,
     query_spaceship: Query<&Transform, (With<Spaceship>, Without<Part>, Without<Boss>)>,
-    cache: Res<Cache>,
     time: Res<Time>,
 ) {
-    if let Ok((mut angular_velocity, id, mut b_transform, mut velocity)) =
-        query_boss.get_single_mut()
-    {
-        if !cache.contains_entity(id) {
-            if let Ok(s_transform) = query_spaceship.get_single() {
-                if !query_boss_edge.is_empty() {
-                    let mut direction =
-                        (s_transform.translation - b_transform.translation).normalize();
-                    let mut rng = rand::thread_rng();
-                    let angle = rng.gen_range(-PI / 2.0..PI / 2.0);
-                    direction = Quat::from_axis_angle(Vec3::Z, angle) * direction;
-                    velocity.0 += ACCELERATION * time.delta_seconds() * direction;
-                    angular_velocity.0 += ROTATION_SPEED * time.delta_seconds();
-                } else {
-                    let direction = (s_transform.translation - b_transform.translation).normalize();
-                    velocity.0 += 2.0 * ACCELERATION * time.delta_seconds() * direction;
-                    angular_velocity.0 += 2.0 * ROTATION_SPEED * time.delta_seconds();
-                }
-            } else {
-                // velocity.0 += Vec3::ZERO;
-                angular_velocity.0 -= ROTATION_SPEED * time.delta_seconds();
-            }
+    if let Ok((mut angular_velocity, mut b_transform, mut velocity)) = query_boss.get_single_mut() {
+        const ACCELERATION: f32 = 500.0;
+        const ROTATION_SPEED: f32 = 20.0;
 
-            velocity.0 *= 1.0 - DRAG;
-            angular_velocity.0 *= 1.0 - ANGULAR_DRAG;
+        if let Ok(s_transform) = query_spaceship.get_single() {
+            if !query_boss_edge.is_empty() {
+                let mut direction = (s_transform.translation - b_transform.translation).normalize();
+                let mut rng = rand::thread_rng();
+                let angle = rng.gen_range(-PI / 2.0..PI / 2.0);
+                direction = Quat::from_axis_angle(Vec3::Z, angle) * direction;
+                velocity.0 += ACCELERATION * time.delta_seconds() * direction;
+                angular_velocity.0 += ROTATION_SPEED * time.delta_seconds();
+            } else {
+                let direction = (s_transform.translation - b_transform.translation).normalize();
+                velocity.0 += 2.0 * ACCELERATION * time.delta_seconds() * direction;
+                angular_velocity.0 += 2.0 * ROTATION_SPEED * time.delta_seconds();
+            }
+        } else {
+            angular_velocity.0 -= ROTATION_SPEED * time.delta_seconds();
         }
+
+        const DRAG: f32 = 0.05;
+        velocity.0 *= 1.0 - DRAG;
+        const ANGULAR_DRAG: f32 = 0.25;
+        angular_velocity.0 *= 1.0 - ANGULAR_DRAG;
 
         b_transform.translation += velocity.0 * time.delta_seconds();
         b_transform.rotation *=
@@ -393,12 +290,24 @@ pub fn attack(
                         continue;
                     }
 
+                    const BLAST_RADIUS: f32 = 15.0;
+                    const BLAST_VERTICES: usize = 32;
+                    const ATTACK_COLOR: Color = Color::RED;
+
                     blast_event.send(BlastEvent {
                         radius: BLAST_RADIUS,
                         vertices: BLAST_VERTICES,
                         color: ATTACK_COLOR,
                         translation: attack_absolute_translation,
                     });
+
+                    const FIRE_IMPACT_RADIUS: f32 = 15.0;
+                    const FIRE_IMPACT_VERTICES: usize = 32;
+                    const FIRE_DAMAGES: u32 = 1;
+                    const FIRE_RADIUS: f32 = 5.0 / FIRE_RANGE as f32;
+                    const FIRE_VERTICES: usize = 32;
+                    const FIRE_RANGE: u32 = 100;
+                    const FIRE_VELOCITY: f32 = 400.0;
 
                     fire_event.send(FireEvent {
                         fire: Fire {
